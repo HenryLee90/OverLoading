264abfe1e2ced1146b8b831939c6ed5f583b7972
Henry Lee <hlee09151@gmail.com>
2022-04-28 10:20:19 -0500

    init

---
+++ OverLoadingMethods/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace OverLoadingMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           // Console.WriteLine("Hello World!");
+            int num1 = 5;
+            int num2 = 19;
+            
+        }
+
+
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+
+
+
+
+    }
+}
