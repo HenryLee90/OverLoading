96c0ea6b710cc7a56df2ade056c4eab5ff469a3d
Henry Lee <hlee09151@gmail.com>
2022-04-29 09:56:12 -0500

    added changes

--- OverLoadingMethods/Program.cs
+++ OverLoadingMethods/Program.cs
@@ -6,21 +6,39 @@ namespace OverLoadingMethods
     {
         static void Main(string[] args)
         {
-           // Console.WriteLine("Hello World!");
-            int num1 = 5;
-            int num2 = 19;
-            
+            // Console.WriteLine("Hello World!");
+            var answer = Add(5, 27);
+            Console.WriteLine(answer);
+            var decimalA = Add(.45m, .76m);
+            Console.WriteLine(decimalA);
+            var thirdM = Add(4, 72, true);
+            var fourthM = Add(5, 86, false);
+            Console.WriteLine(fourthM);
+            Console.WriteLine(thirdM);
         }
-
-
         public static int Add(int num1, int num2)
         {
             return num1 + num2;
+
         }
-
-
-
-
-
+        public static decimal Add(decimal num1, decimal num2)
+        {
+            return num1 + num2;
+        }
+        public static string Add(int num1, int num2, bool isDollars)
+        {
+            int sum = num1 + num2; 
+            // return num1 + num2;
+            if (isDollars == true)
+            {
+                string answer = ($"${sum}");
+                return answer;
+            }
+            else
+            {
+                string answer2 = $"{sum}";
+                return answer2;
+            }
+        }
     }
 }
